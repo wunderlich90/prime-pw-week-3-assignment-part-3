b1fdf4868eb6ce22df24ad8a7a2dd718ebd82ef9
Jeremy Cady <jcady90@icloud.com>
2021-06-24 14:23:10 -0500

    part 3 completed

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -87,3 +87,9 @@ while (changeValue<supplyChanges.length) {
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('9. Total supplies available is:');
+
+let sum = 0;
+for (let i=0; i<supplyChanges.length; i++) {
+  sum += supplyChanges[i];
+}
+console.log(sum);
